0e2aac5ab2a08ceb87ad3a08b8bb3edf6e82b3e4
andcoderweb <andersoncoderweb@gmail.com>
2022-11-29 09:01:51 -0300

    ClientById

--- src/main/java/com/crudClients/resources/ClientResource.java
+++ src/main/java/com/crudClients/resources/ClientResource.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudClients.dto.ClientDTO;
-
 import com.crudClients.services.ClientService;
 
 @RestController
@@ -26,5 +26,17 @@ public class ClientResource {
 	
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<ClientDTO> findById( @PathVariable Long id) {
+		
+		
+		
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	
+	
+	}
+
 
 }
